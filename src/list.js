3b92d72531d19fe1a03ff169185a42311946e3ba
bishal-github <shreesbishal44@gmail.com>
2022-08-22 17:43:42 +0545

    ádding every persons data in list.js

--- src/list.js
+++ src/list.js
@@ -1,9 +1,21 @@
 import React from 'react'
 
-const list = (people) => {
+const list = ({people}) => {
   return (
     <React.Fragment>
-      {people.map(())}
+      {people.map((person)=>{
+        const {id, name, age, image} = person;
+        return(
+<article key={id} className='person'>
+    <img src={image} alt={name} srcset="" />
+    <div>
+        <h4>{name}</h4>
+        <p>{age} years</p>
+    </div>
+
+</article>
+        )
+      })}
     </React.Fragment>
   )
 }
